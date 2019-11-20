d1cb14caa73b33540726d3894478b5b89d5fc13f
Julian Bright <brightsparc@gmail.com>
2019-11-20 04:10:37 +0000

    Updating data science nodes to include SageMaker train and model load impl

--- kedro-tutorial/src/kedro_tutorial/pipelines/data_science/nodes.py
+++ kedro-tutorial/src/kedro_tutorial/pipelines/data_science/nodes.py
@@ -26,7 +26,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import logging
+import os
 from typing import Dict, List
 
 import numpy as np
@@ -34,7 +36,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
-
+from sklearn.externals import joblib
 
 def split_data(data: pd.DataFrame, parameters: Dict) -> List:
     """Splits data into training and test sets.
@@ -92,3 +94,56 @@ def evaluate_model(regressor: LinearRegression, X_test: np.ndarray, y_test: np.n
     score = r2_score(y_test, y_pred)
     logger = logging.getLogger(__name__)
     logger.info("Model has a coefficient R^2 of %.3f.", score)
+    
+if __name__ == '__main__':
+    '''
+    This is the main entry point for sagemaker training job.
+    We need to download data, and call inline methods.
+    '''
+    parser = argparse.ArgumentParser()
+
+    # Hyperparameters are described here. In this simple example we are just including one hyperparameter.
+    parser.add_argument('--test_size', type=float, default=0.2)
+    parser.add_argument('--random_state', type=int, default=3)
+       
+    # Sagemaker specific arguments. Defaults are set in the environment variables.
+    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+
+    # Log the arguments
+    args = parser.parse_args()
+    logger = logging.getLogger(__name__)
+    logger.info("Train arguments ", args)    
+
+    # Take the set of files and read them all into a single pandas dataframe with headers
+    input_files = [ os.path.join(args.train, file) for file in os.listdir(args.train) ]
+    if len(input_files) == 0:
+        raise ValueError(('There are no files in {}.\n' +
+                          'This usually indicates that the channel ({}) was incorrectly specified,\n' +
+                          'the data specification in S3 was incorrectly specified or the role specified\n' +
+                          'does not have permission to access the data.').format(args.train, "train"))
+    raw_data = [ pd.read_csv(file) for file in input_files if file.endswith('csv')]
+    train_data = pd.concat(raw_data)
+
+    # Split train data
+    [X_train, X_test, y_train, y_test] = split_data(train_data, { 
+        'test_size': args.test_size, 'random_state': args.random_state })
+    logger.info("Split records train: %d, test: %d", X_train.shape[0], X_test.shape[0])    
+
+    # Train the model
+    regressor = train_model(X_train, y_train)
+    
+    # Evaluate the model and output coefficients
+    evaluate_model(regressor, X_test, y_test)
+    
+    # Save the model
+    joblib.dump(regressor, os.path.join(args.model_dir, "model.joblib"))
+    
+def model_fn(model_dir):
+    """Deserialized and return fitted model
+    
+    Note that this should have the same name as the serialized model in the main method
+    """
+    regressor = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return regressor    
